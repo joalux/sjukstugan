eb499c5c66c645f529188f1c95459c9ba6d94c4b
joakim lundberg <joakim.lundberg123@gmail.com>
2019-02-28 11:37:06 +0100

    Initial Commit

---
+++ SjukStugan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SjukStugan
+//
+//  Created by joakim lundberg on 2019-02-28.
+//  Copyright © 2019 joakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
