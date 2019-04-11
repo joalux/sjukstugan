109bc5cd9613940db5e9bc4d2931717c84b5c9dc
joakim lundberg <joakim.lundberg123@gmail.com>
2019-04-11 16:38:31 +0200

    fixar med log in, firestore och data

--- SjukStugan/ViewController.swift
+++ SjukStugan/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var newUserLabel: UILabel!
     
     
-    
+    let data: [String: Any] = [:]
      var treatments: [String] = []
     var blue = UIColor(red: 100.0/255.0, green: 130.0/255.0, blue: 230.0/255.0, alpha: 1.0)
     
@@ -43,10 +43,16 @@ class ViewController: UIViewController {
         var newPass = newPassword.text!
         var confirmPass = confirmPassword.text!
         
+        //datab.collection("users").document("new users").collection("treatments").document("new").setData(data)
+
+        
         if newPass == confirmPass || newPass.count > 6 {
             Auth.auth().createUser(withEmail: newName, password: newPass) { authResult, error in
                 if (error == nil) {
                     print("created user")
+                    print(newName)
+                    
+                  
                 } else {
                     print("error!!! \(error)")
                 }
@@ -59,9 +65,17 @@ class ViewController: UIViewController {
     }
     @IBAction func closeSignIn(_ sender: UIButton) {
         self.popOver.removeFromSuperview()
+        
     }
     
     @IBAction func signIn(_ sender: UIButton) {
+        
+        datab = Firestore.firestore()
+        datab.collection("users").document("!!!!!!!!").collection("testing").document("asd").setData(data)
+       
+            
+        
+        
         Auth.auth().signIn(withEmail: userName.text!, password: password.text!) { (user, error) in
             if error == nil{
                 self.performSegue(withIdentifier: "loginToHome", sender: self)
@@ -89,39 +103,25 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        /*let docRef = datab.collection("users").document("user1").collection("treatments").document("mr")
         
-        docRef.getDocument { (document, error) in
-            if let document = document, document.exists {
-                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                print("------------------Document data: \(dataDescription)")
+        super.viewDidLoad()
+        
+        /*datab.collection("treatments").document("LA").setData([
+            "name": "Los Angeles",
+            "state": "CA",
+            "country": "USA"
+        ]) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
             } else {
-                print("Document does not exist")
+                print("Document successfully written!")
             }
         }*/
         
         
-        datab = Firestore.firestore()
-        let usersCollectionRef = datab.collection("users")
-        datab.collection("users").document("user1").collection("treatments").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents {
-                    print("document ID: \(document.documentID)")
-                    print("\(document.documentID) => \(document.data())")
-                    self.treatments.append(document.documentID)
-                    
-                }
-            }
-        }
-        
+        //datab.collection("users").document("user1").setData(data)
        
-        
-        super.viewDidLoad()
-        
-        print("treatments arrayen")
-        print(treatments)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -133,8 +133,7 @@ class ViewController: UIViewController {
                 destination.userName = (userName.text)!
                 print("Going to start totTreatments: \(treatments.count)")
             }
-            /*print(treatments)
-            print("Going to start totTreatments: \(treatments.count)")*/
+            
         }
     }
 
